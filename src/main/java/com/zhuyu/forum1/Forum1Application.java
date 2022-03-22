c75c995f21c81fa688d75f65645aeed79edcdc21
zhuyu <zhuyu@zhuyu.com>
2022-03-22 10:23:09 +0800

    first commit

---
+++ src/main/java/com/zhuyu/forum1/Forum1Application.java
@@ -0,0 +1,13 @@
+package com.zhuyu.forum1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Forum1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Forum1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhuyu/forum1/Forum1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuyu.forum1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Forum1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
